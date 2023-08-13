9f7625b500b1d7d93a46daa267fa93fcd4d5c9c2
github-yst-0001 <1093170697@qq.com>
2023-08-13 10:56:30 +1000

    update server

---
+++ api/server.js
@@ -0,0 +1,24 @@
+import fetch from "cross-fetch";
+
+export default async function handler(request, response) {
+  const { url } = request.query;
+
+  try {
+    const base64_code = await fetch(url)
+      .then(response => {
+        if (!response.ok) {
+          return (`Error: Fetch ${url} failed. Please check your url for base64 decryption.`);
+        }
+        return response.text();
+      })
+      .then(content => {
+        // console.log(content);
+        return content; // Return content to the next .then
+      });
+
+    response.status(200).send(`${base64_code}!`);
+  } catch (error) {
+    console.error(`Error: ${error}`);
+    response.status(500).send("An error occurred while processing the request.");
+  }
+}
