5bf9ae33655c4e7e8e740a7ef9fd42d42e7b93f9
github-yst-0001 <1093170697@qq.com>
2023-08-13 10:01:47 +1000

    Update base64_decode.js

--- api/base64_decode.js
+++ api/base64_decode.js
@@ -1,18 +1,14 @@
-const fetch = require('node-fetch');
-
 module.exports = async (req, res) => {
   const { code } = req.query;
   if (!code) {
-    return res.status(400).send('Missing link to Base64 file');
+    return 'Missing link to Base64 file';
   }
 
   try {
-    const response = await fetch(code);
-    const data = await response.text();
-    const decodedData = Buffer.from(data, 'base64');
-    res.setHeader('Content-Type', response.headers.get('Content-Type'));
-    res.status(200).send(decodedData);
+    const decodedData = Buffer.from(code, 'base64');
+    res.setHeader('Content-Type', 'image/jpeg'); // 根据实际情况设置 Content-Type
+    return decodedData;
   } catch (error) {
-    res.status(500).send('Error decoding Base64 file');
+    return 'Error decoding Base64 file';
   }
 };
